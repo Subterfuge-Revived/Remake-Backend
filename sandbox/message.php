180a8f38de95ff7fa77ec69a431f7130f7f2cf67
Billy <ninja25538@tutanota.de>
2020-02-13 16:19:12 -0500

    Disabled self messages Made it so you can't send messages to yourself, since why would you want to do that?

--- sandbox/message.php
+++ sandbox/message.php
@@ -11,6 +11,7 @@
     $out_profanity = "Diplomacy is not the place for profanity";
     $out_invalid_room = "Player not associated with room";
     $out_invalid_recipient = "Other player not associated with room";
+    $out_same_recipient = "You can't send messages to yourself";
 
 
 try {
@@ -24,27 +25,33 @@ try {
       $func_ses = (new utils_session($db))->reworked_is_session_valid($in_session_id);
       $func_player_id = $func_ses->getPlayerId();
 
-      // Check if player is in Room
-              $db->bind_req($func_player_id, $in_room_id)
-                  ->error_num_row_zero($out_invalid_room)
-                  ->exec_db("
-                  SELECT *
-                  FROM sandbox.player_open_room
-                  WHERE player_id=? AND room_id=?");
-    // Check if the other player is in the room
-              $db->bind_req($in_other_player_id, $in_room_id)
-                  ->error_num_row_zero($out_invalid_recipient)
-                  ->exec_db("
-                  SELECT *
-                  FROM sandbox.player_open_room
-                  WHERE player_id=? AND room_id=?");
-                  
-              // Insert new message
-              $db = new utils_database(utils_database::new_connection());
-              $db->bind_req($in_room_id, time(), $func_player_id, $in_other_player_id, $in_message)
-                  ->exec_db("
-                  INSERT INTO messages (room_id, time_issued, sender_id, recipient_id, message)
-                  VALUES (?,?,?,?,?)");
+      if($func_player_id == $in_other_player_id){
+        throw new \Exception($out_same_recipient);
+      } else {
+
+              // Check if player is in Room
+                      $db->bind_req($func_player_id, $in_room_id)
+                          ->error_num_row_zero($out_invalid_room)
+                          ->exec_db("
+                          SELECT *
+                          FROM sandbox.player_open_room
+                          WHERE player_id=? AND room_id=?");
+            // Check if the other player is in the room
+                      $db->bind_req($in_other_player_id, $in_room_id)
+                          ->error_num_row_zero($out_invalid_recipient)
+                          ->exec_db("
+                          SELECT *
+                          FROM sandbox.player_open_room
+                          WHERE player_id=? AND room_id=?");
+
+                      // Insert new message
+                      $db = new utils_database(utils_database::new_connection());
+                      $db->bind_req($in_room_id, time(), $func_player_id, $in_other_player_id, $in_message)
+                          ->exec_db("
+                          INSERT INTO messages (room_id, time_issued, sender_id, recipient_id, message)
+                          VALUES (?,?,?,?,?)");
+
+      }
 
     }
 } catch (\Exception $e) {
