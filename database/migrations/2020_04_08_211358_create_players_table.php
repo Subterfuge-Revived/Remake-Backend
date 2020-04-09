d5e0810be17b2b8b9083bde412286e4a00c947f1
Alex van de Griendt <alex@e-cube.nl>
2020-04-09 19:22:46 +0200

    Create players migration

---
+++ database/migrations/2020_04_08_211358_create_players_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name'); // NOTE: Renamed from player_name
+            $table->string('password');
+            $table->string('email')->unique()->nullable();
+            $table->integer('rating')->default(1200); // TODO: Is this a good default value?
+            $table->unsignedInteger('wins')->default(0);
+            $table->unsignedInteger('resignations')->default(0); // NOTE: renamed from "resigned"
+            $table->dateTime('last_online_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
