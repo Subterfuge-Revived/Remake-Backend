115809a872ef0b213e91b25f268fb6c35ef78e7e
Alex van de Griendt <alex@e-cube.nl>
2020-04-09 20:07:47 +0200

    Generated auth scaffolding

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/', function(Request $request) {
+
+    $type = $request->get('type');
+
+    if ($type === 'login') {
+
+    }
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
