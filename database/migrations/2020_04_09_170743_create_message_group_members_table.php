009ca3de80e6681047e61b097aaef69bd0618d30
Alex van de Griendt <alex@e-cube.nl>
2020-04-09 19:23:38 +0200

    Create message migrations

---
+++ database/migrations/2020_04_09_170743_create_message_group_members_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessageGroupMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('message_group_members', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('message_group_id');
+
+            $table->foreign('message_group_id')->references('id')->on('message_groups');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('message_group_members', function (Blueprint $table) {
+            $table->dropForeign('message_group_members_message_group_id_foreign');
+        });
+
+        Schema::dropIfExists('message_group_members');
+    }
+}
