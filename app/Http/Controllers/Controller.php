e7997820960fa71da4d4f4b68c87127cc76d9cd1
Alex van de Griendt <alex@e-cube.nl>
2020-04-10 20:47:56 +0200

    Bugfix in setting the session

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -18,6 +18,6 @@ class Controller extends BaseController
 
     public function __construct()
     {
-        $this->session = PlayerSession::findByToken(RequestFacade::input('token'));
+        $this->session = PlayerSession::findByToken(RequestFacade::input('session_id'));
     }
 }
