c3b2c90b640c2c1d4e4a3819732d753ad2d9e660
Alex van de Griendt <alex@e-cube.nl>
2020-04-12 17:13:24 +0200

    Implement the get events method

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -11,6 +11,11 @@ use Illuminate\Validation\ValidationException;
 
 class EventController extends Controller
 {
+    /**
+     * @param Request $request
+     * @return ResponseFactory|Response
+     * @throws ValidationException
+     */
     public function index(Request $request)
     {
         \Validator::make($request->all(), [
@@ -18,6 +23,42 @@ class EventController extends Controller
             'filter' => 'required|string', // Possible values: 'time', 'tick' but maybe others?
             'filter_arg' => 'required|string',
         ])->validate();
+
+        if (!$room = Room::whereId($request->input('room_id'))->first()) {
+            return response('', 404);
+        }
+
+        if (!$room->players->contains($this->session->player)) {
+            throw ValidationException::withMessages(['Player is not in the room']);
+        }
+
+        // TODO: Should we validate on this? The amount of events in a non-started room is zero,
+        // and it seems perfectly fine to return an empty list of events.
+        if (!$room->hasStarted()) {
+            throw ValidationException::withMessages(['Room has not started yet']);
+        }
+
+        // TODO: We should change this API to accept multiple filters
+        $eventsQuery = $room->events();
+        if ($request->input('filter') === 'time') {
+            $eventsQuery = $eventsQuery->where('created_at', '>=', $request->input('filter_arg'));
+        }
+        elseif ($request->input('filter') === 'tick') {
+            $eventsQuery = $eventsQuery->where('occurs_at', '>=', $request->input('filter_arg'));
+        }
+
+        $events = $eventsQuery->get();
+
+        // TODO: We do not return "success: true" in this response, unlike other API calls
+        return response($events->map(function (Event $event) {
+            return [
+                'event_id' => $event->id,
+                'time_issued' => $event->created_at->unix(),
+                'occurs_at' => $event->occurs_at->unix(),
+                'player_id' => $event->player_id,
+                'event_msg' => $event->event_json,
+            ];
+        }));
     }
 
     /**
