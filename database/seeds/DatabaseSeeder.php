cbd27b95fdc268eeb6010efbd1af951427fc5516
Alex van de Griendt <alex@e-cube.nl>
2020-04-15 23:36:03 +0200

    Add seeding for goals

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Goal;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +12,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->addGoals();
+    }
+
+    private function addGoals()
+    {
+        collect([
+            [
+                'identifier' => 'neptunium-200',
+                'description' => 'Be the first to obtain 200kg of Neptunium',
+            ],
+            [
+                'identifier' => 'outposts-15',
+                'description' => 'Be the first to obtain 15 outposts',
+            ],
+        ])->each(function ($goal) {
+            (new Goal($goal))->save();
+        });
     }
 }
