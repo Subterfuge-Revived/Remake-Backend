d8c3168cb195be74c6240281cba2ec0513d01d05
Alex van de Griendt <alex@e-cube.nl>
2020-04-20 22:54:44 +0200

    Add endpoints for message edit and deletion

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Responses\DeletedResponse;
+use App\Http\Responses\UnauthorizedResponse;
+use App\Http\Responses\UpdatedResponse;
 use App\Models\Message;
 use App\Models\MessageGroup;
 use App\Http\Responses\CreatedResponse;
@@ -12,6 +15,31 @@ use mofodojodino\ProfanityFilter\Check as ProfanityCheck;
 
 class MessageController extends Controller
 {
+    /**
+     * Get the messages from the given chat group.
+     *
+     * @param Request $request
+     * @return Response
+     * @throws ValidationException
+     */
+    public function index(Request $request)
+    {
+        $this->validate($request, [
+            'group_id' => 'required|int',
+        ]);
+
+        if (!$group = MessageGroup::whereId($request->input('group_id'))->first()) {
+            throw ValidationException::withMessages(['Message group does not exist']);
+        }
+
+        if (!$group->message_group_members->pluck('player_id')->contains($this->session->player_id)) {
+            throw ValidationException::withMessages(['Player is not in the message group']);
+        }
+
+        $messages = Message::whereMessageGroupId($request->input('group_id'))->get();
+
+        return new Response($messages);
+    }
 
     /**
      * Create a new message.
@@ -22,10 +50,10 @@ class MessageController extends Controller
      */
     public function store(Request $request)
     {
-        \Validator::make($request->all(), [
+        $this->validate($request, [
             'message' => 'required|string',
             'group_id' => 'required|int',
-        ])->validate();
+        ]);
 
         $profanityCheck = new ProfanityCheck();
         if ($profanityCheck->hasProfanity($request->input('message'))) {
@@ -50,28 +78,57 @@ class MessageController extends Controller
     }
 
     /**
-     * Get the messages from the given chat group.
+     * Show a message.
      *
-     * @param Request $request
+     * @param Message $message
      * @return Response
+     */
+    public function show(Message $message)
+    {
+        return new Response($message);
+    }
+
+    /**
+     * Edit a message.
+     * TODO: Determine under which circumstances we should allow this (if at all).
+     *  Right now we allow all players to edit their own messages.
+     *
+     * @param Message $message
+     * @param Request $request
+     * @return UnauthorizedResponse|UpdatedResponse
      * @throws ValidationException
      */
-    public function index(Request $request)
+    public function update(Message $message, Request $request)
     {
-        \Validator::make($request->all(), [
-            'group_id' => 'required|int',
-        ])->validate();
+        $this->validate($request, [
+            'message' => 'required|string',
+        ]);
 
-        if (!$group = MessageGroup::whereId($request->input('group_id'))->first()) {
-            throw ValidationException::withMessages(['Message group does not exist']);
+        if ($message->player != $this->session->player) {
+            return new UnauthorizedResponse();
         }
 
-        if (!$group->message_group_members->pluck('player_id')->contains($this->session->player_id)) {
-            throw ValidationException::withMessages(['Player is not in the message group']);
+        $message->message = $request->get('message');
+        $message->save();
+
+        return new UpdatedResponse($message);
+    }
+
+    /**
+     * Delete a message.
+     * TODO: Determine under which circumstances we should allow this (if at all).
+     *  Right now we allow all players to delete their own messages.
+     * @param Message $message
+     * @return DeletedResponse|UnauthorizedResponse
+     * @throws \Exception
+     */
+    public function destroy(Message $message)
+    {
+        if ($message->player != $this->session->player) {
+            return new UnauthorizedResponse();
         }
 
-        $messages = Message::whereMessageGroupId($request->input('group_id'))->get();
-
-        return new Response($messages);
+        $message->delete();
+        return new DeletedResponse($message);
     }
 }
