822782a2b21ced90b5c95ec478b74bd709bceec5
Alex van de Griendt <alex@e-cube.nl>
2020-04-21 23:30:58 +0200

    Clean up old routes since we won't be using those when using REST

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,6 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\BlockController;
-use App\Http\Controllers\EventController;
-use App\Http\Controllers\GoalController;
-use App\Http\Controllers\MessageController;
-use App\Http\Controllers\MessageGroupController;
-use App\Http\Controllers\RoomController;
-use App\Http\Middleware\AuthenticateAPI;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,77 +13,16 @@ use Illuminate\Validation\ValidationException;
 |
 */
 
-Route::post('/', function (Request $request) {
+Route::post('/register', 'Auth\RegisterController@register');
+Route::post('/login', 'Auth\LoginController@login');
 
-    // FIXME: These should all be different routes and not all registered to the document root.
-    $type = $request->get('type');
+Route::middleware('auth.api')->group(function () {
+    Route::resource('rooms', 'RoomController')->except(['create', 'edit']);
+    Route::resource('blocks', 'BlockController')->except(['create', 'edit']);
+    Route::resource('events', 'EventController')->except(['create', 'edit']);
 
-    if ($type === 'register') {
-        return (new RegisterController())->register($request);
-    }
-    if ($type === 'login') {
-        return (new LoginController())->login($request);
-    }
+    // Next up: message group controller to get a list of messages etc.
+    Route::resource('groups/{group}/messages', 'MessageController')->except(['create', 'edit']);
 
-    // FIXME: This is not the proper way to enforce middleware.
-    // Using different routes will resolve this problem.
-    return app(AuthenticateAPI::class)->handle($request, function (Request $request) use ($type) {
-        if ($type === 'new_room') {
-            return (new RoomController())->store($request);
-        }
-        if ($type === 'join_room') {
-            return (new RoomController())->join($request);
-        }
-        if ($type === 'leave_room') {
-            return (new RoomController())->leave($request);
-        }
-        if ($type === 'start_early') {
-            return (new RoomController())->startEarly($request);
-        }
-        if ($type === 'submit_event') {
-            return (new EventController())->store($request);
-        }
-        if ($type === 'get_events') {
-            return (new EventController())->index($request);
-        }
-        if ($type === 'cancel_event') {
-            return (new EventController())->destroy($request);
-        }
-        if ($type === 'get_room_data') {
-            return (new RoomController())->index($request);
-        }
-        if ($type === 'create_group') {
-            return (new MessageGroupController())->store($request);
-        }
-        if ($type === 'message') {
-            return (new MessageController())->store($request);
-        }
-        if ($type === 'get_message') {
-            return (new MessageController())->index($request);
-        }
-        if ($type === 'block') {
-            return (new BlockController())->store($request);
-        }
-        if ($type === 'unblock') {
-            return (new BlockController())->delete($request);
-        }
-        if ($type === 'get_blocks') {
-            return (new BlockController())->index($request);
-        }
-        if ($type === 'get_goals') {
-            return (new GoalController())->index($request);
-        }
-
-        throw ValidationException::withMessages([
-            "Unexpected request type: $type",
-        ]);
-    });
 });
 
-Route::resource('rooms', 'RoomController')->except(['create', 'edit']);
-Route::resource('blocks', 'BlockController')->except(['create', 'edit']);
-Route::resource('events', 'EventController')->except(['create', 'edit']);
-
-Route::resource('groups/{group}/messages', 'MessageController')->except(['create', 'edit']);
-
-// Next up: message group controller to get a list of messages etc.
