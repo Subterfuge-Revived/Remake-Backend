771c51fb7079ed66574f509ad8dbbba028c67bc5
Alex van de Griendt <alex@e-cube.nl>
2020-04-22 20:58:19 +0200

    Register ValidateNestedResource middleware in the kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -10,12 +10,14 @@ use App\Http\Middleware\EncryptCookies;
 use App\Http\Middleware\RedirectIfAuthenticated;
 use App\Http\Middleware\TrimStrings;
 use App\Http\Middleware\TrustProxies;
+use App\Http\Middleware\ValidateNestedResources;
 use App\Http\Middleware\VerifyCsrfToken;
 use Fruitcake\Cors\HandleCors;
 use Illuminate\Auth\Middleware\AuthenticateWithBasicAuth;
 use Illuminate\Auth\Middleware\Authorize;
 use Illuminate\Auth\Middleware\EnsureEmailIsVerified;
 use Illuminate\Auth\Middleware\RequirePassword;
+use Illuminate\Contracts\Auth\Middleware\AuthenticatesRequests;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 use Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull;
@@ -24,6 +26,7 @@ use Illuminate\Http\Middleware\SetCacheHeaders;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Routing\Middleware\ThrottleRequests;
 use Illuminate\Routing\Middleware\ValidateSignature;
+use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 
@@ -63,8 +66,9 @@ class Kernel extends HttpKernel
 
         'api' => [
             'throttle:60,1',
-            SubstituteBindings::class,
             AddAcceptJsonHeader::class,
+            SubstituteBindings::class,
+            ValidateNestedResources::class,
         ],
     ];
 
