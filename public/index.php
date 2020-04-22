5eca92b86a364f1ef8b206476f646c1f782be95b
Alex van de Griendt <alex@e-cube.nl>
2020-04-22 20:52:10 +0200

    Register the ValidateNestedResource middleware

--- public/index.php
+++ public/index.php
@@ -7,6 +7,9 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
+use App\Http\Kernel;
+use App\Http\Middleware\ValidateNestedResources;
+
 define('LARAVEL_START', microtime(true));
 
 /*
@@ -49,7 +52,9 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 |
 */
 
+/** @var Kernel $kernel */
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$kernel->appendToMiddlewarePriority(ValidateNestedResources::class);
 
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
