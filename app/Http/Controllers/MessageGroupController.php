a48d5812c0088e8d4d80a2f67a36c82193f837f7
Alex van de Griendt <alex@e-cube.nl>
2020-04-22 21:44:25 +0200

    Add RESTful API implementations for messagr groups

--- app/Http/Controllers/MessageGroupController.php
+++ app/Http/Controllers/MessageGroupController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Responses\CreatedResponse;
+use App\Http\Responses\UnauthorizedResponse;
 use App\Models\MessageGroup;
 use App\Models\MessageGroupMember;
 use App\Models\Player;
@@ -14,6 +15,17 @@ use Illuminate\Validation\ValidationException;
 
 class MessageGroupController extends Controller
 {
+
+    /**
+     * Return a list of message groups that the player is in.
+     * @param Request $request
+     * @return Response
+     */
+    public function index(Request $request)
+    {
+        return new Response($this->session->player->message_groups);
+    }
+
     /**
      * Create a new message group.
      *
@@ -68,5 +80,40 @@ class MessageGroupController extends Controller
         $messageGroup->message_group_members()->saveMany($members);
 
         return new CreatedResponse($messageGroup);
+    }
+
+    /**
+     * @param MessageGroup $group
+     * @return Response|UnauthorizedResponse
+     */
+    public function show(MessageGroup $group)
+    {
+        if (!$this->session->player->message_groups->contains($group)) {
+            return new UnauthorizedResponse();
+        }
+
+        return new Response($group);
+    }
+
+    /**
+     * @param MessageGroup $group
+     * @param Request $request
+     * @return UnauthorizedResponse
+     */
+    public function update(MessageGroup $group, Request $request)
+    {
+        // It is nonsensical to update a MessageGroup as it has no real properties on its own.
+        return new UnauthorizedResponse();
+    }
+
+    /**
+     * @param MessageGroup $group
+     * @return UnauthorizedResponse
+     */
+    public function destroy(MessageGroup $group)
+    {
+        // Perhaps an admin should be allowed to delete a chat group?
+        // But it makes no sense for any of the group members to delete the whole chat.
+        return new UnauthorizedResponse();
     }
 }
