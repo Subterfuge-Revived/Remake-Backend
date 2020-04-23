87b5f79816ee559b26a5e7a71d0b040893204595
Alex van de Griendt <alex@e-cube.nl>
2020-04-23 09:55:54 +0200

    Improved nested resource validation by using relationship as defined in the URI

--- app/Http/Middleware/ValidateNestedResources.php
+++ app/Http/Middleware/ValidateNestedResources.php
@@ -18,19 +18,32 @@ class ValidateNestedResources
      */
     public function handle($request, Closure $next, ...$guards)
     {
-        $parentModel = null;
+        $parent = null;
+        $segments = $request->segments();
+
+        // The route may have additional segments on the left side. Therefore,
+        // we have to determine the offset. Note that each bound parameter contributes
+        // two segments: the segment name and the bound id(entifier).
+        // We will subtract these to the total segment count to get the offset.
+        $segmentOffset = count($segments) - 2*count($request->route()->parameters());
+        $parameterIndex = -1;
+
         foreach ($request->route()->parameters() as $parameter) {
-            if (is_null($parentModel)) {
-                $parentModel = $parameter;
+            $parameterIndex++;
+
+            if (is_null($parent)) {
+                $parent = $parameter;
                 continue;
             }
 
-            if (!$parentModel->{$parameter->getTable()}->contains($parameter)) {
+            if (!$parent->{$segments[$segmentOffset + 2*$parameterIndex]}->contains($parameter)) {
                 // The parent model does not have a relation to its child model.
-                // Note that we assume that the relationship is defined on the
-                // parent model and has the same name as the table name of the child.
+                // Note that we assume that the relationship name to be used is
+                // equal to the name of the segment used in the URI.
                 return new NotFoundResponse();
             }
+
+            $parent = $parameter;
         }
 
         return $next($request);
