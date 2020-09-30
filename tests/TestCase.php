5c9e9671451e1705353e6f360844974cb4f20137
Griendt <alex@e-cube.nl>
2020-09-30 19:32:52 +0200

    Add functionality to validate data in unit testing like requests can

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,8 +4,18 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Validator;
+use PHPUnit\Framework\ExpectationFailedException;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication, RefreshDatabase;
+
+    public function assertPassesValidation($data, array $validation) {
+        $validator = Validator::make($data, $validation);
+
+        if ($validator->fails()) {
+            throw new ExpectationFailedException($validator->errors()->toJson());
+        }
+    }
 }
