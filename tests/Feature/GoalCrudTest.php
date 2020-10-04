5bbad95c2a151ed1ed9dd5d8cd3fbead14c995c9
Alex van de Griendt <avdg.lucerna@gmail.com>
2020-10-04 23:14:46 +0200

    Add logged in test for Goal API

--- tests/Feature/GoalCrudTest.php
+++ tests/Feature/GoalCrudTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Feature;
 
-use App\Models\Block;
 use App\Models\Goal;
 use App\Models\Player;
 use App\Models\PlayerSession;
@@ -34,5 +33,20 @@ class GoalCrudTest extends TestCase
             'identifier' => $goal->identifier,
             'description' => $goal->description,
         ]]);
+    }
+
+    public function testFetchingGoalsRequiresLoggingIn()
+    {
+        /** @var Goal $goal */
+        $goal = factory(Goal::class)->make();
+        $goal->save();
+
+        // Not providing a session_id should result in an Unauthorized response
+        $response = $this->get('/api/goals');
+        $response->assertStatus(401);
+
+        // The API should check for an actually valid token, not just any session_id
+        $response = $this->get('/api/goals/session_id=foobar');
+        $response->assertStatus(401);
     }
 }
